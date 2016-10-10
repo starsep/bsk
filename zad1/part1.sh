78b13da48cf7eba08f2ae34307b1c134cfc8c79c
Filip Czaplicki <filipczaplicki@gmail.com>
2016-10-10 17:58:06 +0000

    Adding users if they don't exist, setting some acl

--- zad1/part1.sh
+++ zad1/part1.sh
@@ -3,16 +3,25 @@ class1=`seq 424201 424215`
 class2=`seq 424401 424415`
 class3=`seq 424601 424615`
 function create_user {
-		useradd $1 -d /home/$1 -m
-		mkdir /home/$1/public
+	id -u $1 &> /dev/null || useradd $1 -d /home/$1 -m
+	public=/home/$1/public
+	mkdir -p $public
+	setfacl -m u:master:rwx $public
+	setfacl -d -m u:master:rwx $public
+	getfacl $public
 }
 function create_class {
 	for i in $@; do
-		create_user
+		create_user $i
 	done
 }
 
 create_class $class1
+create_class $class2
+create_class $class3
 
-groupadd staff
-useradd master
+# create group staff if doesn't exist
+groupadd -f staff
+
+# create user master if doesn't exist
+id -u master &> /dev/null || useradd master
