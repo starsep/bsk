17d876f8a14b301a83869481390999abdca7b121
Filip Czaplicki <filipczaplicki@gmail.com>
2016-10-10 20:54:10 +0000

    Seems ok

--- zad1/part1.sh
+++ zad1/part1.sh
@@ -2,23 +2,36 @@
 class1=`seq 424201 424215`
 class2=`seq 424401 424415`
 class3=`seq 424601 424615`
+
 function create_user {
-	id -u $1 &> /dev/null || useradd $1 -d /home/$1 -m
-	public=/home/$1/public
+	username=$1
+	class=$2
+	id -u $username &> /dev/null || useradd $username -d /home/$username -m
+	usermod -a -G $class $username
+	public=/home/$username/public
 	mkdir -p $public
+	chown $username $public
+	chgrp $class $public
 	setfacl -m u:master:rwx $public
 	setfacl -d -m u:master:rwx $public
-	getfacl $public
+	setfacl -m g:staff:rx $public
+	setfacl -d -m g:staff:rwx $public
+	setfacl -m other::0 $public
+	setfacl -d -m other::0 $public
 }
+
 function create_class {
+	class=$1
+	groupadd -f $class
+	shift 1
 	for i in $@; do
-		create_user $i
+		create_user $i $class
 	done
 }
 
-create_class $class1
-create_class $class2
-create_class $class3
+create_class klasa1e $class1
+create_class klasa2e $class2
+create_class klasa3e $class3
 
 # create group staff if doesn't exist
 groupadd -f staff
