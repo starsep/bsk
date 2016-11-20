436f21cd801a9b873d1ab8c2a50f21a9da903d93
Filip Czaplicki <filipczaplicki@gmail.com>
2016-11-20 17:33:58 +0100

    Task 3: blue echo

--- zad3/client.sh
+++ zad3/client.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+function blue_echo {
+  echo -e "\e[1;34m$1\e[0m"
+}
+
 U=fcbsk
+
 apt update
 apt install openssh-server rsync sshfs -y
+
+blue_echo "Creating user $U."
 adduser $U --home /home/$U/
+
+blue_echo "Setting password for $U."
+passwd $U
+
 su $U -c "mkdir -p /home/$U/.ssh"
+
 # Użytkownikowi U na maszynie K utwórz parę kluczy SSH
+blue_echo "Creating ssh key for $U."
 su $U -c "ssh-keygen -N tajnehaslo2 -f /home/$U/.ssh/id_rsa"
--- zad3/server.sh
+++ zad3/server.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
+function blue_echo {
+  echo -e "\e[1;34m$1\e[0m"
+}
+
 # zmienić localhost na adres komputera K
 K=localhost
 U=fcbsk
+STUDENTS=fc359081@students.mimuw.edu.pl
+
 apt update
 apt install openssh-server rsync sshfs -y
 su guest -c "mkdir -p /home/guest/.ssh"
+
+# deploy client.sh
+blue_echo "Copying client.sh to $K"
+scp client.sh guest@$K:~/client.sh
+blue_echo "Executing client.sh @ $K as root!"
+ssh -t guest@$K "sudo /home/guest/client.sh"
+
 # Stwórz guestowi parę kluczy SSH, klucz prywatny chroniony hasłem.
 su guest -c "ssh-keygen -N tajnehaslo -f /home/guest/.ssh/id_rsa"
+
 # Umieść klucz publiczny na komputerze students.
-scp /home/guest/.ssh/id_rsa.pub fc359081@students.mimuw.edu.pl:~/server.pub
+blue_echo "Copying public key to $STUDENTS"
+scp /home/guest/.ssh/id_rsa.pub $STUDENTS:~/server.pub
+blue_echo "Adding public key to authorized_keys @ $STUDENTS"
+ssh $STUDENTS "cat ~/server.pub >> ~/.ssh/authorized_keys && rm server.pub"
 
 # wykonaj czynności umożliwiające U logowanie się z kluczem z K na S
 scp $U@$K:/home/$U/.ssh/id_rsa.pub /home/guest/.ssh/client.pub
-cat /home/guest/.ssh/client.pub >> /home/guest/.ssh/authorized_keys
+su guest -c "cat /home/guest/.ssh/client.pub >> /home/guest/.ssh/authorized_keys"
 rm /home/guest/.ssh/client.pub
+
+# Skonfiguruj klienta ssh tak, aby Twój login na students był używany
+# jako domyślna nazwa użytkownika, gdy guest łączy się ze students
+su guest -c "cat sshconfig >> /home/guest/.ssh/config"
