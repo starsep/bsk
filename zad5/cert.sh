48bc7ccd1994a8b62ab21e069bc7c844933dbe82
Filip Czaplicki <filipczaplicki@gmail.com>
2016-12-17 23:27:49 +0100

    Task 5: SSL fixes.

--- zad5/cert.sh
+++ zad5/cert.sh
@@ -3,6 +3,7 @@ LAB_ID=$(echo -n "$(hostname)" | tail -c 2)
 password=tajnehaslo
 if [ -z "$OPENSSL" ]; then OPENSSL=openssl; fi
 REQ="$OPENSSL req $SSLEAY_CONFIG"
+CA="$OPENSSL ca $SSLEAY_CONFIG"
 DAYS="-days 1461"
 C=PL
 ST=Mazowieckie
@@ -12,8 +13,12 @@ OU=BSK
 CN="bsklab$LAB_ID.mimuw.edu.pl"
 mail="bsk@mimuw.edu.pl"
 REQ_DATA="/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN/emailAddress=$mail"
-$REQ -new -passin pass:$password -passout pass:$password \
-     -keyout newkey.pem -out newreq.pem $DAYS -subj "$REQ_DATA"
+$REQ -new $DAYS -passout pass:$password \
+     -keyout newkey.pem -out newreq.pem -subj "$REQ_DATA"
 echo "Request is in newreq.pem, private key is in newkey.pem"
 openssl rsa -passin pass:$password -in newkey.pem -out newkey.pem
-/usr/lib/ssl/misc/CA.sh -sign
+
+$CA -batch -key $password $DAYS -policy policy_anything \
+    -out newcert.pem -infiles newreq.pem
+cat newcert.pem
+echo "Signed certificate is in newcert.pem"
