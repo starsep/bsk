0082f94fb2e0e575d05400b272291cd2e3af822f
Filip Czaplicki <filipczaplicki@gmail.com>
2017-01-15 17:45:27 +0100

    Task 6: cert using easy-rsa.

--- zad6/cert.sh
+++ zad6/cert.sh
@@ -8,23 +8,6 @@ fi
 name=$1
 email=$2
 
-password=tajnehaslo
-OPENSSL=openssl
-REQ="$OPENSSL req $SSLEAY_CONFIG"
-CA="$OPENSSL ca $SSLEAY_CONFIG"
-DAYS="-days 1461"
-C=PL
-ST=Mazowieckie
-L=Warsaw
-O=MIMUW
-OU=BSK
-mkdir -p certs
-CERT_FILE=certs/$email.pem
-REQ_DATA="/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$name/emailAddress=$email"
-$REQ -new $DAYS -passout pass:$password \
-     -keyout newkey.pem -out newreq.pem -subj "$REQ_DATA"
-openssl rsa -passin pass:$password -in newkey.pem -out newkey.pem
-$CA -batch -key $password $DAYS -policy policy_anything \
-    -out $CERT_FILE -infiles newreq.pem
-rm newkey.pem newreq.pem
-echo "Creating certificate for $name with email $email in $CERT_FILE"
+./easyrsa build-client-full "$name <$email>" nopass
+
+echo "Creating certificate for $name with email $email."
