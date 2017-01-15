08c11684543da65b0741d3e1397d1a2d88284b99
Filip Czaplicki <filipczaplicki@gmail.com>
2017-01-15 18:56:40 +0100

    Task 6: gen-crl, crl conf in server.conf.

--- zad6/run.sh
+++ zad6/run.sh
@@ -4,3 +4,8 @@
 ./easyrsa build-ca nopass
 ./easyrsa gen-dh
 ./cert_users.sh
+./easyrsa build-server-full server nopass
+# sudo openvpn server.conf &
+ln -s pki/private/Filip\ Czaplicki\ \<fc359081@students.mimuw.edu.pl\>.key client.key
+ln -s pki/issued/Filip\ Czaplicki\ \<fc359081@students.mimuw.edu.pl\>.crt client.crt
+./easyrsa gen-crl  
