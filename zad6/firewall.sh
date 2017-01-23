2cb88da1abd9a8d84b14166632590e71a51b3986
Filip Czaplicki <filipczaplicki@gmail.com>
2017-01-23 10:04:23 +0100

    Task 6: firewall added.

---
+++ zad6/firewall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo /sbin/iptables -N VPN_LOCAL
+sudo /sbin/iptables -A VPN_LOCAL --src 192.168.0.0/20 -p tcp --destination-port 1194 -j ACCEPT
+sudo /sbin/iptables -A VPN_LOCAL -p tcp --destination-port 1194 -j REJECT
